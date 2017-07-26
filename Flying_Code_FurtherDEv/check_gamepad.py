6dd38328ec6d560c4c199e3293bfe4587efb6935
f-rower <bitcraze@bitcraze-vm.unassigned-domain>
2017-07-26 18:03:55 +0200

    I have restructured this repo. Now in master I have the 'essential' code (That is, just creating a crazyflie object, connecting to it and sending setpoints to it). Then each branch will be related to an independent development area.

--- Flying_Code_FurtherDEv/check_gamepad.py
+++
@@ -1,6 +0,0 @@
-import gamepad
-import time
-
-while 1:
-    print((gamepad.get()[14]))
-    time.sleep(0.5)
--- Flying_Code_Logging/pid_param.py
+++
@@ -1,4 +0,0 @@
-'''File containing the PID parameters for each case'''
-flying = {'kp':1}
-landing = {'kp':2}
-perching ={'kp':1}
